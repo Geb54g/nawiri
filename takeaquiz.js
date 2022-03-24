7b5b707e080e67809996409ba6a7eeda548899f6
Geb54g <gabriel.ndolo@student.moringaschool.com>
2022-03-24 15:29:22 +0300

    st:added functionality

---
+++ takeaquiz.js
@@ -0,0 +1,16 @@
+
+const button = document.getElementById("btn1")  
+
+button.addEventListener("click", (e)=> {
+    e.preventDefault()
+    send()
+})
+    function send(){
+        let message = document.getElementById("quiz")
+        alert("You results are being processed kindly we will notify you shortly")
+        
+        //details.style.display = 'none';
+        //quiz.innerHTML = ".";
+        
+        }
+
