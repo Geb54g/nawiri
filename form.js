f7ac1aad3553aa9a6c5ec799cfceaf99dedb0bb5
Christine Mwangi <christine.mwangi@student.moringaschool.com>
2022-03-24 11:50:36 +0300

    feat:added function

---
+++ form.js
@@ -0,0 +1,14 @@
+
+
+const button = document.getElementById("btn")
+button.addEventListener("click", (e)=> {
+   e.preventDefault()
+  send()
+})
+function send(){
+let message = document.getElementById("pay")
+
+//details.style.display = 'none';
+pay.innerHTML = "PAY FOR THE SESSION (KSH.1500) <br>MPESA TILL NO:24545 ACCOUNT NO:YOUR NAME .";
+
+}
